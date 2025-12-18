6b0e8082915e0f8dc987cec31482f3a5fcce109d
Salvo-Raia <salvo.raia@outlook.it>
2025-12-18 18:06:37 +0100

    Add Bonus 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,9 +16,20 @@ console.log("Array con ordine invertito:", reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >=5) {
+    longNames.push(teachers[i]);
+    console.log(teachers[i], "contiene 5 o più caratteri. Lo aggiungo alla lista");
+  }
+}
+
+console.table(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(5, 1)
+console.table(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
